1e95dab7a76557cc3da099be3191f0f514b0e311
Akshat Dasondhi <85483154+AkshatDasondhi@users.noreply.github.com>
2023-06-12 10:24:06 +0530

    Delete BandNameGen.py

--- BandNameGen.py
+++
@@ -1,6 +0,0 @@
-print("Welcome to the Band Name Generator.")
-print("What's the name of city you grew in ?")
-city = input()
-print("What's your pet's name ?")
-pet = input()
-print("Your Band name could be %s %s" % (city,pet))
